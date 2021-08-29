4fde5cbaf6f807ec213641e9fe9d2e74d3340b06
Olena Sevastian <olena.sevastian@altexsoft.com>
2021-08-29 20:21:44 +0300

    make functions async

--- product-service/src/functions/getProductsById.js
+++ product-service/src/functions/getProductsById.js
@@ -2,14 +2,15 @@ import productsList from '../mockData/productList.json';
 
 export default async (event) => {
   const productId = event.pathParameters.productId;
+  const productsJSON = await JSON.stringify(
+    productsList.find((product) => product.id === productId)
+  );
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(
-      productsList.find((product) => product.id === productId)
-    ),
+    body: productsJSON,
   };
 };
--- product-service/src/functions/getProductsList.js
+++ product-service/src/functions/getProductsList.js
@@ -1,12 +1,13 @@
 import productsList from '../mockData/productList.json';
 
 export default async () => {
+  const productsJSON = await JSON.stringify(productsList);
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(productsList),
+    body: productsJSON,
   };
 };

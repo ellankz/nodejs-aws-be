affa939f9cc7cea7c3ac088c5f5e67b61a1f6dc9
Olena Sevastian <olena.sevastian@altexsoft.com>
2021-08-29 20:24:59 +0300

    handle not found error

--- product-service/src/functions/getProductsById.js
+++ product-service/src/functions/getProductsById.js
@@ -2,15 +2,29 @@ import productsList from '../mockData/productList.json';
 
 export default async (event) => {
   const productId = event.pathParameters.productId;
-  const productsJSON = await JSON.stringify(
-    productsList.find((product) => product.id === productId)
-  );
+  const product = productsList.find((product) => product.id === productId);
+
+  if (!product) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: {
+        code: 404,
+        status: 'Not found',
+      },
+    };
+  }
+
+  const productJSON = await JSON.stringify(product);
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: productsJSON,
+    body: productJSON,
   };
 };
--- product-service/src/functions/getProductsList.js
+++ product-service/src/functions/getProductsList.js
@@ -1,6 +1,19 @@
 import productsList from '../mockData/productList.json';
 
 export default async () => {
+  if (!productsList && !productsList.length) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: {
+        code: 404,
+        status: 'Not found',
+      },
+    };
+  }
   const productsJSON = await JSON.stringify(productsList);
   return {
     statusCode: 200,
